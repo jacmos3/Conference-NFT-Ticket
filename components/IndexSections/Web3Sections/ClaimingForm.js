eef0de008224d4b09f575ae473e3c5ed903c055f
Jacopo <jacopogithub.pbcc5@aleeas.com>
2023-01-26 14:51:10 +0100

    added little traveler image on discount message, and fixed eth to matic

--- components/IndexSections/Web3Sections/ClaimingForm.js
+++ components/IndexSections/Web3Sections/ClaimingForm.js
@@ -261,7 +261,7 @@ class ClaimingForm extends Component{
                 <Input
                   label={{ basic: true, content: this.state.chain.coin, id:"inputLabel" }}
                   labelPosition='right'
-                  placeholder='Ether amount'
+                  placeholder='Matic amount'
                   readOnly
                   value = {this.state.adjustedPrice}
                 />
@@ -270,9 +270,15 @@ class ClaimingForm extends Component{
                 <Form.Field className={`${styles.content}`} >
                 <h4>NOTE:</h4>
                 {this.state.isOwningLittleTraveler ?
-                  <p>You are enjoying a {this.state.lTPercentageDiscount}% discount because you own a <a className={`a__underline__primary`} target="_blank" href={this.props.state.lnk_littleTraveler}>Little Traveler NFT</a></p>
+                  <p className="">You are enjoying a {this.state.lTPercentageDiscount}% discount because you own a
+                    <a className={`a__underline__primary`} target="_blank" href={this.props.state.lnk_littleTraveler}> Little Traveler NFT
+                    <img src = "../../lt.png" alt = "Little Traveler Collection"/></a>
+                  </p>
                   :
-                  <p>Get a {this.state.lTPercentageDiscount}% discount on ticket price by purchasing a <a className={`a__underline__primary`} target="_blank" href={this.props.state.lnk_littleTraveler}>Little Traveler NFT</a> first</p>
+                  <p>Get a {this.state.lTPercentageDiscount}% discount on ticket price by purchasing a
+                    <a className={`a__underline__primary`} target="_blank" href={this.props.state.lnk_littleTraveler}> Little Traveler NFT</a> first
+                      <a className={`a__underline__primary`} target="_blank" href={this.props.state.lnk_littleTraveler}><img src = "../../lt.png" alt = "Little Traveler Collection"/></a>
+                  </p>
                 }
                 <h3>Upgrade to <a className={`a__underline__primary`} target="_blank" href={this.props.state.lnk_airdrop}>AIRDROP TICKET</a>:</h3>
                 <div>Pay 20% extra (optional) to get airdrops of the speaker's projects tokens</div>
